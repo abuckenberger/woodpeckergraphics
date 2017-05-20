6fdf9e06f0a1dc9be58c20764c1c7a7e3c4abb29
abuckenberger <abuckenberger@eagles.ewu.edu>
2017-05-20 13:46:12 -0700

    AIRTABLE IS GOING TO TAKE OVER THE WORLD
    *SO impressed
    *can’t stop smiling
    *it works
    *i got a thing to work

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -10419,5 +10419,43 @@ module.exports = Airtable;
 // @codekit-prepend "airtable.js";
 
 
-console.log('Hello, World!');
+//handshake
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyqGtSAOwpL9x1Tx'}).base('appX7l2d6izOrMORA');
+
+
+
+
+//make sure airtable is working
+
+//console.log(base);
+
+
+base('Products').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 100,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Product'));
+        
+        var products = `<a href="https://airtable.com/shrvX80fabWt0v5Fq">${record.get('Product')}</a>`
+        var options = `<p>${record.get('Options')}</p>`
+        
+        //console.log(options);
+        
+        $('#products').append(products, options);
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
 
